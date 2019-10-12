fce00a9d47c60cc1095fccf09d5a8dc044aabb68
Garrett Chan <garrett.chan@salesforce.com>
2019-10-12 16:12:28 -0700

    model changes

--- hw3/homework3/homework/models.py
+++ hw3/homework3/homework/models.py
@@ -4,32 +4,30 @@ import torch.nn as nn
 
 
 class CNNClassifier(nn.Module):
-    '''
     def __init__(self):
         super(CNNClassifier, self).__init__()
-        self.conv1 = nn.Conv2d(3, 24, 3)
-        self.conv2 = nn.Conv2d(24, 16, 3)
+        self.conv1 = nn.Conv2d(3, 36, 3, padding=1)
+        self.conv1_1 = nn.Conv2d(36, 36, 3, padding=1)
+        self.conv2 = nn.Conv2d(36, 24, 3, padding=1)
+        self.conv2_1 = nn.Conv2d(24, 24, 3, padding=1)
+        self.conv3 = nn.Conv2d(24, 12, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.linear1 = nn.Linear(16 * 14 * 14, 256)
-        self.linear2 = nn.Linear(256, 32)
+        self.linear1 = nn.Linear(24 * 16 * 16, 1024)
+        self.linear2 = nn.Linear(1024, 32)
         self.linear3 = nn.Linear(32, 6)
-    '''
-
-    def __init__(self):
-        super(CNNClassifier, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 3)
-        self.conv2 = nn.Conv2d(64, 32, 3)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.linear1 = nn.Linear(32 * 14 * 14, 256)
-        self.linear2 = nn.Linear(256, 32)
-        self.linear3 = nn.Linear(32, 6)
+        self.flag = True
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.pool(F.relu(x))
-        x = self.conv2(x)
-        x = self.pool(F.relu(x))
-        x = x.view(-1, 14 * 14 * 32)
+        x = self.conv1(x) # 36 x 64 x 64
+        x = self.conv1_1(F.relu(x)) # 36 x 64 x 364
+        x = self.pool(F.relu(x)) # 36 x 32 x 32
+        x = self.conv2(F.relu(x)) # 24 x 32 x 32
+        x = self.conv2_1(F.relu(x)) # 24 x 32 x 32
+        x = self.pool(F.relu(x)) # 24 x 16 x 16
+        #x = self.conv3(F.relu(x))
+        #x = self.conv3(F.relu(x))
+        #x = self.pool(F.relu(x))
+        x = x.view(-1, 24 * 16 * 16)
         x = self.linear1(F.relu(x))
         x = self.linear2(F.relu(x))
         x = self.linear3(F.relu(x))
